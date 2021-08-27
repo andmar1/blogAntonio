45569c47de92a0bcb7b764e6b400867b769b260d
Antonio Andrade <antoniomaresmares001gmail.com>
2021-08-27 01:21:46 -0500

    first version

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// mostrar y ocultar menu
+
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// mostrar
+toggleMenu.addEventListener('click',()=>{
+    navMenu.classList.toggle('show')
+});
+
+// ocultar
+closeMenu.addEventListener('click',()=>{
+    navMenu.classList.remove('show')
+});
+
+
+// remove menu al momento de habiltar una opcion de la barra
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// scroll section active link 
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll',scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop -50
+        sectionId = current.getAttribute('id')
+        
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+        }
+        else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
+        }
+    })
+}
+
+
